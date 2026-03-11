f58c776ae638ce89034c82665e38c26a3d2250bd
Benoit Bourgeon <benoit.bourgeon.ext@primetals.com>
2026-03-11 15:22:00 +0100

    deployed sepolia

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -33,18 +33,3 @@ const config: HardhatUserConfig = {
 };
 
 export default config;
-
-// Tell Hardhat to use the local soljson.js instead of downloading
-const {
-  TASK_COMPILE_SOLIDITY_GET_SOLC_BUILD,
-} = require("hardhat/builtin-tasks/task-names");
-const { subtask } = require("hardhat/config");
-
-subtask(TASK_COMPILE_SOLIDITY_GET_SOLC_BUILD, async (args: any) => {
-  return {
-    compilerPath: require.resolve("solc/soljson.js"),
-    isSolcJs: true,
-    version: "0.8.28",
-    longVersion: "0.8.28",
-  };
-});
