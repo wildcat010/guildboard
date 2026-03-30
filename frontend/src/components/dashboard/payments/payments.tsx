ceca4f7a8a530cb80d78fb36b07675f3f8f5ee75
benoit bourgeon <benoit.bourgeon@yahoo.fr>
2026-03-30 18:00:33 +0200

    upgrade

--- frontend/src/components/dashboard/payments/payments.tsx
+++ frontend/src/components/dashboard/payments/payments.tsx
@@ -3,15 +3,29 @@
 import styles from "./payments.module.css";
 import { useEffect } from "react";
 import { Section } from "./../../../constants/constants";
+import { useBalance } from "wagmi";
+import { GUILDBOARD_ADDRESS } from "@/contracts";
 
 export default function Payments() {
+  const { data: balance } = useBalance({
+    address: GUILDBOARD_ADDRESS,
+  });
   return (
     <div className={styles.content}>
       <div className={`${styles.pageHeader} ${styles.animateIn}`}>
         <div>
           <div className={styles.pageTitle}>Payments </div>
         </div>
-        <button className={styles.btnPrimary}>+ Create Payment</button>
+        <div className={styles.buttonWraper}>
+          <button className={styles.btnPrimary}>+ Create Payment</button>
+          <button className={styles.btnPrimary}>+ Create Deposit</button>
+        </div>
+      </div>
+      <div className={styles.pageSub}>
+        Contract Balance
+        <p className={styles.text}>
+          {balance?.formatted} {balance?.symbol}
+        </p>
       </div>
     </div>
   );
