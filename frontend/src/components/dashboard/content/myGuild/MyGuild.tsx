f54ef1e64c5afcc9c0d79050c7868c3a3ae30717
benoit bourgeon <benoit.bourgeon@yahoo.fr>
2026-04-01 20:39:59 +0200

    updates

--- frontend/src/components/dashboard/content/myGuild/MyGuild.tsx
+++ frontend/src/components/dashboard/content/myGuild/MyGuild.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import styles from "./payments.module.css";
+import styles from "./MyGuild.module.css";
 import { useEffect, useState } from "react";
 
 import { useBalance } from "wagmi";
@@ -9,5 +9,32 @@ import { GUILDBOARD_ADDRESS } from "@/contracts";
 import { useDeposit } from "@/hooks/useDeposit";
 
 export default function MyGuild() {
-  return <>MyGuild</>;
+  return (
+    <>
+      <div className={styles.content}>
+        <div className={`${styles.pageHeader} ${styles.animateIn}`}>
+          <div>
+            <div className={styles.pageTitle}>My Guild -</div>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "5px",
+            }}
+          >
+            <button className={styles.btnPrimary}>+ Create Guild</button>
+          </div>
+        </div>
+        <div className={styles.pageGuilds}>{/* //CARD HERE */}</div>
+
+        {/* {showModalCreateGuild && (
+        <AddGuildModal
+          onClose={() => setShowModalCreateGuild(false)}
+          onSuccess={handleOnSuccess}
+        />
+      )} */}
+      </div>
+    </>
+  );
 }
